e6b2c3361e1c908bde8543149934cbb85d87edea
shakibh10 <billalsikdar03@gmail.com>
2023-06-14 18:31:26 +0600

    first commit

---
+++ coderoj5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int value;
+
+    scanf("%d",&value);
+
+    printf("\t%d",value);
+    printf("\t%o",value);
+    printf("\t%X",value);
+
+    return 0;
+
+}
